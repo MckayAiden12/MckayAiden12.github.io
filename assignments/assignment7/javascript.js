4423352f13c6d52ac46c05e6dc2c5bf2b8178771
MckayAiden12 <112011143+MckayAiden12@users.noreply.github.com>
2023-09-24 02:37:21 -0400

    fixed some bugs in javascript

--- assignments/assignment7/javascript.js
+++ assignments/assignment7/javascript.js
@@ -12,22 +12,58 @@ function showExercise(exerciseId) {
     }
 }
 
+let isMenuVisible = false; // Track the visibility state of the menu
+
 function toggleMenu() {
-    const menu = document.getElementById('menu');
-    menu.style.display = menu.style.display === 'block' ? 'none' : 'block';
+    const exercise1Content = document.getElementById('exercise1-content');
+    const exercise2Content = document.getElementById('exercise2-content');
+
+    if (!isMenuVisible) {
+        exercise1Content.style.display = 'block'; // Show Exercise 1 content
+        exercise2Content.style.display = 'block'; // Show Exercise 2 content
+        isMenuVisible = true; // Set the menu visibility state to true
+    } else {
+        exercise1Content.style.display = 'none'; // Hide Exercise 1 content
+        exercise2Content.style.display = 'none'; // Hide Exercise 2 content
+        isMenuVisible = false; // Set the menu visibility state to false
+    }
 }
 
+window.addEventListener('load', function() {
+    // Hide Exercise 1 and Exercise 2 content on small screens by default
+    if (window.innerWidth <= 768) {
+        const exercise1Content = document.getElementById('exercise1-content');
+        const exercise2Content = document.getElementById('exercise2-content');
+        exercise1Content.style.display = 'none';
+        exercise2Content.style.display = 'none';
+    }
+
+});
+
 function determineOrder() {
+    const name1 = document.getElementById('name1').value;
     const age1 = parseInt(document.getElementById('age1').value);
+    const name2 = document.getElementById('name2').value;
     const age2 = parseInt(document.getElementById('age2').value);
+    const name3 = document.getElementById('name3').value;
     const age3 = parseInt(document.getElementById('age3').value);
 
     if (isNaN(age1) || isNaN(age2) || isNaN(age3)) {
         document.getElementById('order-result').innerHTML = "Please enter valid ages.";
     } else {
-        const ages = [age1, age2, age3];
-        ages.sort((a, b) => b - a);
-        document.getElementById('order-result').innerHTML = "Ordered ages: " + ages.join(', ');
+        const people = [
+            { name: name1, age: age1 },
+            { name: name2, age: age2 },
+            { name: name3, age: age3 }
+        ];
+
+        // Sort the people array by age in descending order
+        people.sort((a, b) => b.age - a.age);
+
+        // Extract the names in the sorted order
+        const sortedNames = people.map(person => person.name);
+
+        document.getElementById('order-result').innerHTML = "Ordered names by age: " + sortedNames.join(', ');
     }
 }
 
