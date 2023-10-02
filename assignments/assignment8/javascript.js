7d6b05a85f3a08b93acead46ce2321cffe58321c
MckayAiden12 <112011143+MckayAiden12@users.noreply.github.com>
2023-10-01 22:51:56 -0400

    added homework8 debugging

--- assignments/assignment8/javascript.js
+++ assignments/assignment8/javascript.js
@@ -13,3 +13,47 @@ function updateThermometer() {
         alert('Please enter a valid number.');
     }
 }
+
+const runningMan = document.getElementById('runningMan');
+let isRunning = false;
+let isWalking = true;
+let animationFrame;
+const maxMargin = window.innerWidth - 1000; // Adjust this value as needed
+
+runningMan.addEventListener('click', () => {
+    if (isWalking) {
+        isWalking = false;
+        animateRunningMan();
+    } else {
+        isWalking = true;
+        cancelAnimationFrame(animationFrame);
+    }
+});
+
+function animateRunningMan() {
+    if (!isWalking) {
+        const currentMargin = parseInt(getComputedStyle(runningMan).marginLeft) || 0;
+        const incrementValue = 10; // Adjust this value for speed
+        const delayBetweenImages = 100; // Adjust this value for smoother transition
+
+        const newMargin = currentMargin + incrementValue;
+
+        // Check if newMargin exceeds the maximum limit
+        if (newMargin <= maxMargin) {
+            runningMan.style.marginLeft = `${newMargin}px`;
+        } else {
+            runningMan.style.marginLeft = `${maxMargin}px`;
+            isWalking = true; // Stop when the box ends
+        }
+
+        if (!isWalking) {
+            runningMan.src = 'running-man.png';
+            setTimeout(() => {
+                runningMan.src = 'walking-man.png';
+                animateRunningMan();
+            }, delayBetweenImages);
+        }
+
+        animationFrame = requestAnimationFrame(animateRunningMan);
+    }
+}
